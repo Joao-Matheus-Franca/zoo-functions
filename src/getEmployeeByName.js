e458040762b46e7dbbf9c16129a0cd9df5229e91
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-28 17:52:06 -0300

    Concluido: requisito 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const nomeArray = employeeName.split(' ');
+  const novo = employees.filter((emp) =>
+    emp.firstName === nomeArray[0] || emp.lastName === nomeArray[0]);
+  return novo[0];
 }
 
 module.exports = getEmployeeByName;
