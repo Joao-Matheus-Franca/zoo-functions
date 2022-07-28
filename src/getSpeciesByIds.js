039335141605261325c4044eed078cf81c5badf9
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-28 17:08:59 -0300

    Concluido: requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return ids;
+  }
+  if (ids.length === 1) {
+    const unica = species.find((element) => element.id === ids[0]);
+    return [unica];
+  }
+  return species.filter((element, index) => element.id === ids[index]);
 }
 
-module.exports = getSpeciesByIds; 
+module.exports = getSpeciesByIds;
