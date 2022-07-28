e1a85b1d95d8aee09d98571d9469b1180229c74d
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-28 18:20:19 -0300

    Concluido: requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((emp) => emp.managers[0] === id || emp.managers[1] === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const sub = employees.filter((emp) =>
+    emp.managers[0] === managerId || emp.managers[1] === managerId);
+  return sub.map((nome) => `${nome.firstName} ${nome.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
