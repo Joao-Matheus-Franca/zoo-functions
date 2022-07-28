387990b33e48167ab0f03122f09ca6788f09100d
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-28 20:53:23 -0300

    Concluido: requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const result = {};
+    species.forEach((sp) => {
+      const { name, residents } = sp;
+      result[name] = residents.length;
+    });
+    return result;
+  }
+  const objeto = (species.find((sp) => sp.name === animal.specie)).residents;
+  if (animal.sex === undefined) {
+    return objeto.length;
+  }
+  if (animal.sex !== undefined) {
+    const number = objeto.filter((sex) => sex.sex === animal.sex);
+    return number.length;
+  }
 }
 
 module.exports = countAnimals;
