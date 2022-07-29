9e92a1c27b155fc9eb6df0556a3dea226dcb665f
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-28 23:00:17 -0300

    Concluido: requisito 06 e 07

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,22 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  test('Testando de handlerElephants é uma função', () => {
+    expect(typeof handlerElephants).toEqual('function');
+  });
+  test('Testando de handlerElephants recebe apenas strings', () => {
+    expect(handlerElephants(20)).toEqual('Parâmetro inválido, é necessário uma string');
+  });
+  test('Testando de handlerElephants retorna Undefined ao não receber parâmetros', () => {
+    expect(handlerElephants()).toEqual(undefined);
+  });
+  test('Testando se o retorno da função é o esperado', () => {
+    expect(handlerElephants('count')).toEqual(4);
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+    expect(handlerElephants('location')).toEqual('NW');
+    expect(handlerElephants('popularity')).toEqual(5);
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+    expect(handlerElephants('teste')).toEqual(null);
+  });
+});
