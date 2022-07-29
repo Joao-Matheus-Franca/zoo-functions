0dc7e115589ef94d1a75fcbe91b64519f1c09eff
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-29 00:21:49 -0300

    Concluido: requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const result = { child: 0,
+    adult: 0,
+    senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      result.child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      result.adult += 1;
+    } else {
+      result.senior += 1;
+    }
+  });
+  return result;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.keys === undefined) {
+    return 0;
+  }
+  const result = countEntrants(entrants);
+  const { adult, senior, child } = prices;
+  let total = 0;
+  total = (result.child * child) + (result.adult * adult) + (result.senior * senior);
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
