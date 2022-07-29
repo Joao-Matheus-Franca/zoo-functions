b26a5025495fc664e4f22d26912780615a4b5a60
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-29 12:17:26 -0300

    Concluido: requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const object = employees.find((emp) => emp.id === id);
+  const animals = object.responsibleFor[0];
+  const animal = species.find((element) => element.id === animals);
+  const names = animal.residents;
+  const ordenado = names.sort((a, b) => b.age - a.age);
+  return Object.values(ordenado[0]);
 }
 
 module.exports = getOldestFromFirstSpecies;
