88c449c7ae0ef6f2bdef595e5d0acdea600744bc
João Matheus Silva Franca <matheussfranca@live.com>
2022-07-31 19:44:04 -0300

    Concluido: requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,127 @@
 const data = require('../data/zoo_data');
 
+const animalsNe = data.species.filter((element) => element.location === 'NE');
+const animalsNw = data.species.filter((element) => element.location === 'NW');
+const animalsSe = data.species.filter((element) => element.location === 'SE');
+const animalsSw = data.species.filter((element) => element.location === 'SW');
+
+const object = {
+  NE: animalsNe,
+  NW: animalsNw,
+  SE: animalsSe,
+  SW: animalsSw,
+};
+
+const noEntries = () => {
+  object.NE = animalsNe.map((element) => element.name);
+  object.NW = animalsNw.map((element) => element.name);
+  object.SE = animalsSe.map((element) => element.name);
+  object.SW = animalsSw.map((element) => element.name);
+};
+
+const withEntries = (options) => {
+  object.NE = animalsNe.map((element) => ({
+    [element.name]: element.residents.map((rs) => rs.name),
+  }));
+  object.NW = animalsNw.map((element) => ({
+    [element.name]: element.residents.map((rs) => rs.name),
+  }));
+  object.SE = animalsSe.map((element) => ({
+    [element.name]: element.residents.map((rs) => rs.name),
+  }));
+  object.SW = animalsSw.map((element) => ({
+    [element.name]: element.residents.map((rs) => rs.name),
+  }));
+};
+
+const withSort = () => {
+  object.NE = animalsNe.map((element) => ({
+    [element.name]: element.residents.map((rs) => rs.name)
+      .sort(),
+  }));
+  object.NW = animalsNw.map((element) => ({
+    [element.name]: element.residents.map((rs) => rs.name)
+      .sort(),
+  }));
+  object.SE = animalsSe.map((element) => ({
+    [element.name]: element.residents.map((rs) => rs.name)
+      .sort(),
+  }));
+  object.SW = animalsSw.map((element) => ({
+    [element.name]: element.residents.map((rs) => rs.name)
+      .sort(),
+  }));
+};
+
+const withSex = (options) => {
+  object.NE = animalsNe.map((element) => ({
+    [element.name]: element.residents.filter((resi) => resi.sex === options.sex)
+      .map((rs) => rs.name),
+  }));
+  object.NW = animalsNw.map((element) => ({
+    [element.name]: element.residents.filter((resi) => resi.sex === options.sex)
+      .map((rs) => rs.name),
+  }));
+  object.SE = animalsSe.map((element) => ({
+    [element.name]: element.residents.filter((resi) => resi.sex === options.sex)
+      .map((rs) => rs.name),
+  }));
+  object.SW = animalsSw.map((element) => ({
+    [element.name]: element.residents.filter((resi) => resi.sex === options.sex)
+      .map((rs) => rs.name),
+  }));
+};
+
+const withSexSorted = (options) => {
+  object.NE = animalsNe.map((element) => ({
+    [element.name]: element.residents.filter((resi) => resi.sex === options.sex)
+      .map((rs) => rs.name).sort(),
+  }));
+  object.NW = animalsNw.map((element) => ({
+    [element.name]: element.residents.filter((resi) => resi.sex === options.sex)
+      .map((rs) => rs.name).sort(),
+  }));
+  object.SE = animalsSe.map((element) => ({
+    [element.name]: element.residents.filter((resi) => resi.sex === options.sex)
+      .map((rs) => rs.name).sort(),
+  }));
+  object.SW = animalsSw.map((element) => ({
+    [element.name]: element.residents.filter((resi) => resi.sex === options.sex)
+      .map((rs) => rs.name).sort(),
+  }));
+};
+
+const verifyOptions = (options) => {
+  if (options.includeNames && options.sex === undefined) {
+    withEntries();
+  }
+  if (options.sorted && options.sex === undefined) {
+    withSort();
+  }
+};
+
+const verifyWithSex = (options) => {
+  if (options.sorted === undefined) {
+    withSex(options);
+  }
+  if (options.sorted) {
+    withSexSorted(options);
+  }
+};
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options || options.includeNames === undefined) {
+    noEntries();
+    return object;
+  }
+  if (options.sex === undefined) {
+    verifyOptions(options);
+    return object;
+  }
+  if (options.sex) {
+    verifyWithSex(options);
+    return object;
+  }
 }
 
 module.exports = getAnimalMap;
